a03d4dfe7d867613b1de4358f493018b60bf0c46
Mayank Tiwari <mayank_tiwari@cable.comcast.com>
2019-09-19 14:06:43 -0400

    Added sample configurations

---
+++ examples/juniper/go_get_netconf_json.go
@@ -0,0 +1,51 @@
+// Go NETCONF Client - Example
+//
+// Copyright (c) 2013-2018, Juniper Networks, Inc. All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
+package main
+
+import (
+	"fmt"
+	"log"
+
+	"github.com/Juniper/go-netconf/netconf"
+	"golang.org/x/crypto/ssh"
+)
+
+func main() {
+	sshConfig := &ssh.ClientConfig{
+		User:            "sample",
+		Auth:            []ssh.AuthMethod{ssh.Password("sample")},
+		HostKeyCallback: ssh.InsecureIgnoreHostKey(),
+	}
+
+	s, err := netconf.DialSSH("127.0.0.1:22", sshConfig)
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	defer s.Close()
+
+	fmt.Println(s.ServerCapabilities)
+	fmt.Println(s.SessionID)
+
+	// Sends raw XML
+
+	// Get chassis inventory; return data is XML
+	//reply, err := s.Exec(netconf.RawMethod("<get-chassis-inventory/>"))
+
+	// show bgp summary; return data is JSON
+	//reply, err := s.Exec(netconf.RawMethod("<get-config format=\"json\"/>"))
+	//reply, err := s.Exec(netconf.RawMethod("<get-config><source><running/></source></get-config>"))
+	//reply, err := s.Exec(netconf.RawMethod("<get-config> <source><running/></source> <filter type=\"subtree\"> <configuration> <system/> </configuration> </filter> </get-config>"))
+	//reply, err := s.Exec(netconf.RawMethod("<get-config format=\"json\"> <source><running/></source> <filter type=\"subtree\"> <configuration> <protocols> <mld/> </protocols></configuration> </filter> </get-config>"))
+	reply, err := s.Exec(netconf.RawMethod("<get-configuration database=\"committed\" format=\"json\"><configuration><protocols><mld/></protocols></configuration></get-configuration>"))
+
+	if err != nil {
+		panic(err)
+	}
+	fmt.Printf("Reply: %+v", reply)
+}
---
+++ go.mod
@@ -0,0 +1,8 @@
+module github.com/Juniper/go-netconf
+
+go 1.12
+
+require (
+	github.com/google/go-cmp v0.3.1
+	golang.org/x/crypto v0.0.0-20190911031432-227b76d455e7
+)
